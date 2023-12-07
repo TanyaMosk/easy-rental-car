31a12e984dc700eb8e215a1f25569589d6526de3
TanyaMosk <tanya856227@gmail.com>
2023-12-07 21:49:04 +0100

    Update 7-12

--- src/redux/advertsSlice.js
+++ src/redux/advertsSlice.js
@@ -1,30 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { buildQueries } from "@testing-library/react";
 import { fetchCars } from "./operations";
 
 const handleFetchCars = (state, action) => {
   state.adverts = action.payload;
 };
-
-const initState = [
-  {
-    year: 0,
-    make: "",
-    model: "",
-    type: "",
-    img: "",
-    description: "",
-    fuelConsumption: "",
-    engineSize: "",
-    accessories: [],
-    functionalities: [],
-    rentalPrice: "",
-    rentalCompany: "",
-    address: "",
-    rentalConditions: "",
-    mileage: 0,
-  },
-];
 
 const advertsSlice = createSlice({
   name: "adverts",
